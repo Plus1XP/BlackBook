9a4c4bdcd3a8d076f169ae2badd15c38b8bf8774
Plus1XP <evldev@protonmail.ch>
2020-02-08 20:32:29 +0000

    Feat(UI): Add dark modern UI theme
    Includes bindings for Minimize, Maximize and Close buttons

---
+++ BlackBook/Models/RelayCommands.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BlackBook.Models
+{
+    public class Command : ICommand
+    {
+        protected readonly Func<Boolean> canExecute;
+
+        protected readonly Action execute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this.canExecute != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+
+            remove
+            {
+                if (this.canExecute != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+
+        public Command(Action execute, Func<Boolean> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public Command(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        public virtual Boolean CanExecute(Object parameter)
+        {
+            return this.canExecute == null ? true : this.canExecute();
+        }
+
+        public virtual void Execute(Object parameter)
+        {
+            this.execute();
+        }
+    }
+
+    //public class Command<T> : ICommand
+    //{
+    //    protected readonly Func<Boolean> canExecute;
+
+    //    protected readonly Action<T> execute;
+
+    //    public event EventHandler CanExecuteChanged
+    //    {
+    //        add
+    //        {
+    //            if (this.canExecute != null)
+    //            {
+    //                CommandManager.RequerySuggested += value;
+    //            }
+    //        }
+
+    //        remove
+    //        {
+    //            if (this.canExecute != null)
+    //            {
+    //                CommandManager.RequerySuggested -= value;
+    //            }
+    //        }
+    //    }
+
+    //    public Command(Action<T> execute, Func<Boolean> canExecute)
+    //    {
+    //        if (execute == null)
+    //        {
+    //            throw new ArgumentNullException("execute");
+    //        }
+    //        this.execute = execute;
+    //        this.canExecute = canExecute;
+    //    }
+
+    //    public Command(Action<T> execute)
+    //        : this(execute, null)
+    //    {
+    //    }
+
+    //    public virtual Boolean CanExecute(Object parameter)
+    //    {
+    //        return this.canExecute == null ? true : this.canExecute();
+    //    }
+
+    //    public virtual void Execute(Object parameter)
+    //    {
+    //        this.execute((T)parameter);
+    //    }
+    //}
+
+    public class Command<T> : ICommand
+    {
+        protected readonly Func<T, Boolean> canExecute;
+
+        protected readonly Action<T> execute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this.canExecute != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+
+            remove
+            {
+                if (this.canExecute != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+
+        public Command(Action<T> execute, Func<T, Boolean> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public Command(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        public virtual Boolean CanExecute(Object parameter)
+        {
+            return this.canExecute == null ? true : this.canExecute((T)parameter);
+        }
+
+        public virtual void Execute(Object parameter)
+        {
+            this.execute((T)parameter);
+        }
+    }
+
+    public class AsyncCommand : Command
+    {
+        private bool isExecuting = false;
+
+        public event EventHandler Started;
+
+        public event EventHandler Ended;
+
+        public bool IsExecuting
+        {
+            get { return this.isExecuting; }
+        }
+
+        public AsyncCommand(Action execute, Func<Boolean> canExecute)
+            : base(execute, canExecute)
+        {
+        }
+
+        public AsyncCommand(Action execute)
+            : base(execute)
+        {
+        }
+
+        public override Boolean CanExecute(Object parameter)
+        {
+            return ((base.CanExecute(parameter)) && (!this.isExecuting));
+        }
+
+        public override void Execute(object parameter)
+        {
+            try
+            {
+                this.isExecuting = true;
+                if (this.Started != null)
+                {
+                    this.Started(this, EventArgs.Empty);
+                }
+
+                Task task = Task.Factory.StartNew(() =>
+                {
+                    this.execute();
+                });
+                task.ContinueWith(t =>
+                {
+                    this.OnRunWorkerCompleted(EventArgs.Empty);
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+            }
+            catch (Exception ex)
+            {
+                this.OnRunWorkerCompleted(new RunWorkerCompletedEventArgs(null, ex, true));
+            }
+        }
+
+        private void OnRunWorkerCompleted(EventArgs e)
+        {
+            this.isExecuting = false;
+            if (this.Ended != null)
+            {
+                this.Ended(this, e);
+            }
+        }
+    }
+}
---
+++ BlackBook/ViewModels/MainViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using BlackBook.Models;
+
+namespace BlackBook.ViewModels
+{
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.CloseWindowCommand = new Command<Window>(this.CloseWindow);
+            this.MaximizeWindowCommand= new Command<Window>(this.MaximizeWindow);
+            this.MinimizeWindowCommand = new Command<Window>(this.MinimizeWindow);
+            this.RestoreWindowCommand = new Command<Window>(this.RestoreWindow);
+        }
+
+        public Command<Window> CloseWindowCommand { get; set; }
+        public Command<Window> MaximizeWindowCommand { get; set; }
+        public Command<Window> MinimizeWindowCommand { get; set; }
+        public Command<Window> RestoreWindowCommand { get; set; }
+
+        private void CloseWindow(Window window)
+        {
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
+
+        private void MaximizeWindow(Window window)
+        {
+            if (window.ResizeMode == ResizeMode.CanResize || window.ResizeMode == ResizeMode.CanResizeWithGrip)
+            {
+                window.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void MinimizeWindow(Window window)
+        {
+            if (window.ResizeMode != ResizeMode.NoResize)
+            {
+                window.WindowState = WindowState.Minimized;
+            }
+        }
+
+        private void RestoreWindow(Window window)
+        {
+            if (window.ResizeMode == ResizeMode.CanResize || window.ResizeMode == ResizeMode.CanResizeWithGrip)
+            {
+                window.WindowState = WindowState.Normal;
+            }
+        }
+
+        private bool CanResizeWindow(Window window)
+        {
+            return window.ResizeMode == ResizeMode.CanResize || window.ResizeMode == ResizeMode.CanResizeWithGrip;
+        }
+
+        private bool CanMinimizeWindow(Window window)
+        {
+            return window.ResizeMode != ResizeMode.NoResize;
+        }
+    }
+}
