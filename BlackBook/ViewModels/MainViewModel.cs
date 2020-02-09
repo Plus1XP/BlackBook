e1f2b96830d8d1d80065c45f92281bdfebbe1109
Plus1XP <evldev@protonmail.ch>
2020-02-09 19:03:31 +0000

    Feat(UI): Add canExecute bools to mainViewModel

--- BlackBook/ViewModels/MainViewModel.cs
+++ BlackBook/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,15 @@ namespace BlackBook.ViewModels
 {
     class MainViewModel
     {
+        private bool canResizeWindow { get; set; } = true;
+        private bool canMinimizeWindow { get; set; } = true;
+
         public MainViewModel()
         {
             this.CloseWindowCommand = new Command<Window>(this.CloseWindow);
-            this.MaximizeWindowCommand= new Command<Window>(this.MaximizeWindow);
-            this.MinimizeWindowCommand = new Command<Window>(this.MinimizeWindow);
-            this.RestoreWindowCommand = new Command<Window>(this.RestoreWindow);
+            this.MaximizeWindowCommand = new Command<Window>(this.MaximizeWindow, o => this.canResizeWindow);
+            this.MinimizeWindowCommand = new Command<Window>(this.MinimizeWindow, o => this.canMinimizeWindow);
+            this.RestoreWindowCommand = new Command<Window>(this.RestoreWindow, o => this.canResizeWindow);
         }
 
         public Command<Window> CloseWindowCommand { get; set; }
@@ -34,7 +38,7 @@ namespace BlackBook.ViewModels
 
         private void MaximizeWindow(Window window)
         {
-            if (window.ResizeMode == ResizeMode.CanResize || window.ResizeMode == ResizeMode.CanResizeWithGrip)
+            if (this.CanResizeWindow(window))
             {
                 window.WindowState = WindowState.Maximized;
             }
@@ -42,7 +46,7 @@ namespace BlackBook.ViewModels
 
         private void MinimizeWindow(Window window)
         {
-            if (window.ResizeMode != ResizeMode.NoResize)
+            if (this.CanMinimizeWindow(window))
             {
                 window.WindowState = WindowState.Minimized;
             }
@@ -50,7 +54,7 @@ namespace BlackBook.ViewModels
 
         private void RestoreWindow(Window window)
         {
-            if (window.ResizeMode == ResizeMode.CanResize || window.ResizeMode == ResizeMode.CanResizeWithGrip)
+            if (this.CanResizeWindow(window))
             {
                 window.WindowState = WindowState.Normal;
             }
