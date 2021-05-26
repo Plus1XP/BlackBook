8bacb039878bc9988946230203a8cb0e0b38f0ac
Plus1XP <evldev@protonmail.ch>
2021-05-26 10:28:44 +0100

    Feat(Model): Add Profile Interface to BlackEngine

---
+++ BlackEngine/Models/IProfile.cs
@@ -0,0 +1,20 @@
+﻿namespace BlackEngine.Models
+{
+    public interface IProfile
+    {
+        int ID { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Alias { get; set; }
+        string Location { get; set; }
+        int? Age { get; set; }
+        string DOB { get; set; }
+        string Mobile { get; set; }
+        string Twitter { get; set; }
+        string Instagram { get; set; }
+        string Facebook { get; set; }
+        string Snapchat { get; set; }
+        string AltSocialMedia { get; set; }
+
+    }
+}
