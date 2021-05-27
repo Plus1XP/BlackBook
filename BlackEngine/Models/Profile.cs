c28e453bacff2c7cd0775ba5d789fbab5c2d17b5
Plus1XP <evldev@protonmail.ch>
2021-05-27 11:29:10 +0100

    Feat(Model): Add Profile Class to BlackEngine

---
+++ BlackEngine/Models/Profile.cs
@@ -0,0 +1,19 @@
+﻿namespace BlackEngine.Models
+{
+    public class Profile : IProfile
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Alias { get; set; }
+        public string Location { get; set; }
+        public int? Age { get; set; }
+        public string DOB { get; set; }
+        public string Mobile { get; set; }
+        public string Twitter { get; set; }
+        public string Instagram { get; set; }
+        public string Facebook { get; set; }
+        public string Snapchat { get; set; }
+        public string AltSocialMedia { get; set; }
+    }
+}
