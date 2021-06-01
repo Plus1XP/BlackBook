34e8064efa143c552d30a4b4c0d6d5de17b53d1f
Plus1XP <evldev@protonmail.ch>
2021-06-01 11:34:36 +0100

    Feat(Model): Add Main viewmodel to BlackUI

---
+++ BlackUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+
+using BlackUI.Models;
+
+namespace BlackUI.ViewModels
+{
+    class MainViewModel
+    {
+        private bool canResizeWindow { get; set; } = true;
+        private bool canMinimizeWindow { get; set; } = true;
+
+        public MainViewModel()
+        {
+            this.CloseWindowCommand = new Command<Window>(this.CloseWindow);
+            this.MaximizeWindowCommand = new Command<Window>(this.MaximizeWindow, o => this.canResizeWindow);
+            this.MinimizeWindowCommand = new Command<Window>(this.MinimizeWindow, o => this.canMinimizeWindow);
+            this.RestoreWindowCommand = new Command<Window>(this.RestoreWindow, o => this.canResizeWindow);
+        }
+
+        public Command<Window> CloseWindowCommand { get; set; }
+        public Command<Window> MaximizeWindowCommand { get; set; }
+        public Command<Window> MinimizeWindowCommand { get; set; }
+        public Command<Window> RestoreWindowCommand { get; set; }
+
+        private void CloseWindow(Window window)
+        {
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
+
+        private void MaximizeWindow(Window window)
+        {
+            if (this.CanResizeWindow(window))
+            {
+                window.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void MinimizeWindow(Window window)
+        {
+            if (this.CanMinimizeWindow(window))
+            {
+                window.WindowState = WindowState.Minimized;
+            }
+        }
+
+        private void RestoreWindow(Window window)
+        {
+            if (this.CanResizeWindow(window))
+            {
+                window.WindowState = WindowState.Normal;
+            }
+        }
+
+        private bool CanResizeWindow(Window window)
+        {
+            return window.ResizeMode == ResizeMode.CanResize || window.ResizeMode == ResizeMode.CanResizeWithGrip;
+        }
+
+        private bool CanMinimizeWindow(Window window)
+        {
+            return window.ResizeMode != ResizeMode.NoResize;
+        }
+    }
+}
