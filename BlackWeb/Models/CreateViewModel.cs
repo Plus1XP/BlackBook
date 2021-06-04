3995288bee58548b391e2233b1332685affc97aa
Plus1XP <evldev@protonmail.ch>
2021-06-04 10:48:10 +0100

    Feat(Model): Add CreatViewModel to BlackWeb

---
+++ BlackWeb/Models/CreateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BlackEngine.DataAccess;
+using BlackEngine.Models;
+
+namespace BlackWeb.Models
+{
+    public class CreateViewModel
+    {
+        SqlDataAccess dataAccess;
+
+        public CreateViewModel(string connectionString)
+        {
+            dataAccess = new SqlDataAccess(connectionString);
+        }
+
+        public void CreateProfile(IProfile profile)
+        {
+            dataAccess.SaveData(profile);
+        }
+    }
+}
