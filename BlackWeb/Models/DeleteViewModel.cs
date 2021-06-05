9531f7fe8baa907a0465f75ba70a1ac7e787bb52
Plus1XP <evldev@protonmail.ch>
2021-06-05 10:48:30 +0100

    Feat(Model): Add DeleteViewModel to BlackWeb

---
+++ BlackWeb/Models/DeleteViewModel.cs
@@ -0,0 +1,24 @@
+﻿using BlackEngine.DataAccess;
+using BlackEngine.Models;
+
+using System.Collections.Generic;
+
+namespace BlackWeb.Models
+{
+    public class DeleteViewModel
+    {
+        SqlDataAccess dataAccess;
+
+        public DeleteViewModel(string connectionString)
+        {
+            dataAccess = new SqlDataAccess(connectionString);
+        }
+
+        public List<IProfile> GetProfiles => dataAccess.LoadData();
+
+        public void DeleteProfile(IProfile profile)
+        {
+            dataAccess.DeleteData(profile);
+        }
+    }
+}
