ffe4d41cfbd3df38096055f37539b7dcfeb792b3
Plus1XP <evldev@protonmail.ch>
2021-06-07 08:49:20 +0100

    Feat(Model): Add EditViewModel to BlackWeb

---
+++ BlackWeb/Models/EditViewModel.cs
@@ -0,0 +1,24 @@
+﻿using BlackEngine.DataAccess;
+using BlackEngine.Models;
+
+using System.Collections.Generic;
+
+namespace BlackWeb.Models
+{
+    public class EditViewModel
+    {
+        SqlDataAccess dataAccess;
+
+        public EditViewModel(string connectionString)
+        {
+            dataAccess = new SqlDataAccess(connectionString);
+        }
+
+        public List<IProfile> GetProfiles => dataAccess.LoadData();
+
+        public void EditProfile(IProfile profile)
+        {
+            dataAccess.EditData(profile);
+        }
+    }
+}
