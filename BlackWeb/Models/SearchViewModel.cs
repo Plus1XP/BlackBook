cfe3faac9e1fb9208936698f01c0f324749bd678
Plus1XP <evldev@protonmail.ch>
2021-06-10 08:50:22 +0100

    Feat(Model): Add SearchViewModel to BlackWeb

---
+++ BlackWeb/Models/SearchViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BlackEngine.DataAccess;
+using BlackEngine.Models;
+
+using System.Collections.Generic;
+
+namespace BlackWeb.Models
+{
+    public class SearchViewModel
+    {
+        SqlDataAccess dataAccess;
+
+        public SearchViewModel(string connectionString)
+        {
+            dataAccess = new SqlDataAccess(connectionString);
+        }
+
+        public List<IProfile> GetProfiles => dataAccess.LoadData();
+    }
+}
